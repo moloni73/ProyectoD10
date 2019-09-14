e52e34b2a97761992eeb854525ddd7544921e26c
Carlos Molina <carlos170373@gmail.com>
2019-09-14 14:26:41 +0000

    cambio Id

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,22 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
+var numcarta = Math.floor(Math.random() * 12) + 0;
+
+function raimag() {
+  var imagarray = [];
+  imagarray[0] = "&spades"; //espadas
+  imagarray[1] = "&clubs"; //trebol
+  imagarray[2] = "&hearts"; //Corazones
+  imagarray[3] = "&diams"; //diamantes
+  var i = Math.floor(Math.random() * 3) + 0;
+  return imagarray[i];
+}
+
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  // alert(raimag());
+
+  document.getElementById("pinta-top").innerHTML = raimag();
+  document.getElementById("numero").innerHTML = numcarta;
+  document-getElementById("pinta-down").innerHTML = raimag();
 };
